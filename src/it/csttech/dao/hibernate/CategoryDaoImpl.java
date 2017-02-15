200738c5a0806986905846757988d52a25fb3ec4
carolina.martone <carolina.martone@CST-121017-02.csttech.it>
2017-02-15 17:07:52 +0100

    ho aggiunto il get e set della sessionfactory

--- src/it/csttech/dao/hibernate/CategoryDaoImpl.java
+++ src/it/csttech/dao/hibernate/CategoryDaoImpl.java
@@ -1,5 +1,7 @@
 package it.csttech.dao.hibernate;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import it.csttech.dao.CategoryDao;
@@ -9,7 +11,15 @@ import it.csttech.model.Category;
 @Repository
 public class CategoryDaoImpl extends BaseDaoImpl<Category> implements CategoryDao
 {
-    //final indica che l'oggetto non può essere ricreato,ma può essere modificato
+    @Autowired
+    private SessionFactory sessionFactory; 
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+}
+
+public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+}//final indica che l'oggetto non può essere ricreato,ma può essere modificato
     //static dipende solo dalla classe e non dall'istanza è uguale per tutte le istanze.
     
 
